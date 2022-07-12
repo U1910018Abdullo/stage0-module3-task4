0bcafd6cc2614d18cf49732c5c48705bbd1a887b
U1910018Abdullo <1910018.abdullo.kholmakhmatov@gmail.com>
2022-07-12 18:03:54 +0500

    task4 is done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c='c';
+        int cNum=c;
+        char h='h';
+        int hNum=h;
+        char a='a';
+        int aNum=a;
+        char r='r';
+        int rNum=r;
+
+        System.out.println(cNum+"\n"+hNum+"\n"+aNum+"\n"+rNum);
+
     }
 }
